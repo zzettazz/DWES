4c74ebae17856501a503b886c6a42c2ee179bb96
zzettazz <junquerdaviddaw@gmail.com>
2023-10-20 13:07:07 +0200

    ej 9

---
+++ Cookies/Ejercicio9/login.php
@@ -0,0 +1,10 @@
+
+
+<h1>LOGIN</h1>
+<form action="listaUsuarios.php" method="GET">
+    Usuario <input type="text" id="nombreUsuarioIntroducido" name="nombreUsuarioIntroducido"/><br/>
+    Contraseña <input type="password" id="contraseñaIntroducida" name="contraseñaIntroducida"/><br/>
+    Recordar <input type="checkbox" name="recordar"/><br/>
+    <input type="submit"/><br/><br/>
+    <a href="registrar.php">Registrar nuevo usuario</a>
+</form>
