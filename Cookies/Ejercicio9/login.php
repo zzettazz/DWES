e9b670d0e49b58180a2cc15c118421739f0fcd13
zzettazz <junquerdaviddaw@gmail.com>
2023-10-20 13:59:20 +0200

    a

--- Cookies/Ejercicio9/login.php
+++ Cookies/Ejercicio9/login.php
@@ -1,19 +1,8 @@
-<?php
-session_start();
-$usuarioGuadado = "";
-if (!isset($_SESSION["usuarios"]))
-{
-    $_SESSION["usuarios"] = array(
-        "arrayUsuarios" => array(),
-        "arrayContrasenias" => array()
-    );
-}
-?>
 
 <h1>LOGIN</h1>
-<form action="listaUsuarios.php" method="GET">
-    Usuario <input type="text" id="nombreUsuarioIntroducido" name="nombreUsuarioIntroducido"/><br/>
-    Contraseña <input type="password" id="contraseñaIntroducida" name="contraseñaIntroducida"/><br/>
+<form action="hacerLogin.php" method="GET">
+    Usuario <input type="text" id="nombreUsuarioIntroducido" name="nombreUsuarioIntroducido" value=<?=$ultimoUsuario?>/><br/>
+    Contraseña <input type="password" id="contraseniaIntroducida" name="contraseniaIntroducida"/><br/>
     Recordar <input type="checkbox" name="recordar"/><br/>
     <input type="submit"/><br/><br/>
     <a href="registrar.php">Registrar nuevo usuario</a>
---
+++ Cookies/Ejercicio9/registrar.php
@@ -0,0 +1,7 @@
+<h1>NUEVO USUARIO</h1>
+<br/>
+<form action="registarNuevoUsuario.php" method="GET">
+    Usuario <input type="text" id="usuarioRegistro" name="usuarioRegistro"/><br/>
+    Contraseña <input type="password" id="contraseniaRegistro" name="contraseniaRegistro"><br/>
+    <inpu type="submit"/>
+</form>
