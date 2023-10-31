ae991dfa26124fb289d60d22143c95bbabf8bcd8
zzettazz <junquerdaviddaw@gmail.com>
2023-10-31 18:20:32 +0100

    a

--- Cajon Desastre/variados/Sesiones/procesar.php
+++ Cajon Desastre/variados/Sesiones/procesar.php
@@ -5,7 +5,7 @@ if (isset($_POST["usuario"]) && isset($_POST["contraseña"]) && $_POST["usuario"
 {
     $_SESSION["usuario"] = $_POST["usuario"];
     $_SESSION["contraseña"] = $_POST["contraseña"];
-    echo "<h1>HOLA</h1>";
+    echo "<h1>LOGIN CORRECTO</h1>";
     header("refresh:3;url=resultado.php");
 }
 else
