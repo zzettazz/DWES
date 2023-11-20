acf463a4eb1a21c4365bb9ad05e65821485324df
zzettazz <junquerdaviddaw@gmail.com>
2023-11-20 08:56:18 +0100

    redBean

--- TEMA 8 - Persistencia de objetos/Ejercicios/EjercicioPrueba/tempCodeRunnerFile.php
+++ TEMA 8 - Persistencia de objetos/Ejercicios/EjercicioPrueba/tempCodeRunnerFile.php
@@ -3,9 +3,6 @@ require __DIR__ . '/../../../lib/rb.php';
 
 R::setup('mysql:host=localhost;port=3333;dbname=pruebas', 'root', '');
 
-// Crear la tabla 'prueba' con la columna 'id'
-R::exec("CREATE TABLE IF NOT EXISTS prueba (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, valor1 VARCHAR(255), c2 VARCHAR(255))");
-
 $tabla = R::dispense('prueba');
 
 $valor1 = "Nuevo valor 1";
@@ -15,4 +12,5 @@ $tabla->valor1 = $valor1;
 $tabla->c2 = $columna;
 
 R::store($tabla);
+
 ?>
